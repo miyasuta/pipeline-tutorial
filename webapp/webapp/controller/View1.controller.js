0eab5c5534ab28d377e16ab935681cb0b8490b21
Yasutake <mio.fujita.01@gmail.com>
2021-01-31 21:11:24 +0000

    failed test

--- webapp/webapp/controller/View1.controller.js
+++ webapp/webapp/controller/View1.controller.js
@@ -10,6 +10,21 @@ sap.ui.define([
 		return Controller.extend("sap.cp.webapp.controller.View1", {
 			onInit: function () {
 
-			}
+            },
+            
+            priceState: function (iPrice) {
+                if (iPrice < 50) {
+                    return "Success";
+                }
+                else if (iPrice >= 50 && iPrice < 250) {
+                    return "None";
+                }
+                else if (iPrice >= 250 && iPrice < 2000) {
+                    return "Warning";
+                }
+                // else {
+                //     return "Error";
+                // }
+            }
 		});
 	});
--- webapp/webapp/test/unit/controller/View1.controller.js
+++ webapp/webapp/test/unit/controller/View1.controller.js
@@ -3,7 +3,7 @@
 sap.ui.define([
 	"sap/cp/webapp/controller/View1.controller"
 ], function (Controller) {
-	"use strict";
+    "use strict";
 
 	QUnit.module("View1 Controller");
 
@@ -11,6 +11,57 @@ sap.ui.define([
 		var oAppController = new Controller();
 		oAppController.onInit();
 		assert.ok(oAppController);
-	});
+    });
+
+    var oController = new Controller();
+    
+    QUnit.module("Price State");
+    function priceStateTestCase(oOptions) {
+        // Act 
+        var sState = oController.priceState(oOptions.price);
+
+        // Assert
+        oOptions.assert.strictEqual(sState, oOptions.expected, "The price state was correct");
+    }
+
+    QUnit.test("Should format the products with a price lower than 50 to Success", function (assert) {
+        priceStateTestCase.call(this, {
+            assert: assert,
+            price: 42,
+            expected: "Success"
+        });
+    });    
+
+    QUnit.test("Should format the products with a price of 50 to None", function (assert) {
+        priceStateTestCase.call(this, {
+            assert: assert,
+            price: 50,
+            expected: "None"
+        });
+    });
+
+    QUnit.test("Should format the products with a price between 50 and 250 to None", function (assert) {
+        priceStateTestCase.call(this, {
+            assert: assert,
+            price: 112,
+            expected: "None"
+        });
+    });
+
+    QUnit.test("Should format the products with a price between 250 and 2000 to Warning", function (assert) {
+        priceStateTestCase.call(this, {
+            assert: assert,
+            price: 798,
+            expected: "Warning"
+        });
+    });    
+
+    QUnit.test("Should format the products with a price higher than 2000 to Error", function (assert) {
+        priceStateTestCase.call(this, {
+            assert: assert,
+            price: 2001,
+            expected: "Error"
+        });
+    });    
 
 });
